8534b1eba2c70d8a792b5ea268eb6e86065ab19f
FranzouilleWazaah <franz.alliod@viacesi.fr>
2016-06-15 16:33:53 +0200

    Tests Views --> Laurann

---
+++ view/src/main/java/view/LaurannView.java
@@ -0,0 +1,71 @@
+package view;
+
+import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.io.File;
+import javax.swing.SwingUtilities;
+
+import contract.ControllerOrder;
+import contract.IController;
+import contract.IModel;
+import contract.IView;
+
+/**
+ * The Class View.
+ *
+ * @author Franz Alliod
+ */
+public class LaurannView implements IView, Runnable {
+
+	/** The frame. */
+	private final ViewFrame viewFrame;
+
+	/**
+	 * Instantiates a new view.
+	 *
+	 * @param model
+	 *          the model
+	 */
+	public LaurannView(final IModel model) {
+		this.viewFrame = new ViewFrame(model);
+		SwingUtilities.invokeLater(this);
+		}
+
+	/**
+	 * Launch the frame
+	 * set the frame visible
+	 * @param true
+	 */
+	public void openFrame() {
+	this.viewFrame.setVisible(true);
+		}
+
+	/**
+	 * Sets the controller.
+	 *
+	 * @param controller
+	 *          the new controller
+	 */
+	public void setController(final IController controller) {
+		this.viewFrame.setController(controller);
+		}
+	/**
+	 * Close the frame
+	 * @dispose
+	 * destroy JFrame object
+	 */
+	 public void closeFrame() {
+	
+	 this.viewFrame.setVisible(false);
+	 } 
+/**
+ * Add all sprites 1 per 1 to build each level
+ */
+		
+	 	public void drawMap()
+{
+}
+}
---
+++ view/src/main/java/view/LaurannViewFrame.java
@@ -0,0 +1,151 @@
+package view;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import contract.IController;
+import contract.IModel;
+
+/**
+ * The Class ViewFrame.
+ *
+ * @author Franz Alliod
+ */
+class LaurannViewFrame extends JFrame implements KeyListener {
+
+	/** The model. */
+	private IModel						model;
+
+	/** The controller. */
+	private IController				controller;
+	/** The Constant serialVersionUID. */
+	private static final long	serialVersionUID	= -697358409737458175L;
+
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model
+	 *          the model
+	 * @throws HeadlessException
+	 *           the headless exception
+	 */
+	public ViewFrame(final IModel model) throws HeadlessException {
+		this.buildViewFrame(model);
+	}
+
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model
+	 *          the model
+	 * @param gc
+	 *          the gc
+	 */
+	public ViewFrame(final IModel model, final GraphicsConfiguration gc) {
+		super(gc);
+		this.buildViewFrame(model);
+	}
+
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model
+	 *          the model
+	 * @param title
+	 *          the title
+	 * @throws HeadlessException
+	 *           the headless exception
+	 */
+	public ViewFrame(final IModel model, final String title) throws HeadlessException {
+		super(title);
+		this.buildViewFrame(model);
+	}
+
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model
+	 *          the model
+	 * @param title
+	 *          the title
+	 * @param gc
+	 *          the gc
+	 */
+	public ViewFrame(final IModel model, final String title, final GraphicsConfiguration gc) {
+		super(title, gc);
+		this.buildViewFrame(model);
+	}
+
+	/**
+	 * Gets the controller.
+	 *
+	 * @return the controller
+	 */
+	private IController getController() {
+		return this.controller;
+	}
+
+	/**
+	 * Sets the controller.
+	 *
+	 * @param controller
+	 *          the new controller
+	 */
+	protected void setController(final IController controller) {
+		this.controller = controller;
+	}
+
+	/**
+	 * Gets the model.
+	 *
+	 * @return the model
+	 */
+	protected IModel getModel() {
+		return this.model;
+	}
+
+	/**
+	 * Sets the model.
+	 *
+	 * @param model
+	 *          the new model
+	 */
+	private void setModel(final IModel model) {
+		this.model = model;
+	}
+
+	/**
+	 * Builds the view frame.
+	 *
+	 * @param model
+	 *          the model
+	 */
+	private void buildViewFrame(final IModel model) {
+		this.setModel(model);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.addKeyListener(this);
+		this.setContentPane(new ViewPanel(this));
+		this.setSize(400 + this.getInsets().left + this.getInsets().right, 60 + this.getInsets().top + this.getInsets().bottom);
+		this.setLocationRelativeTo(null);
+	}
+
+	
+	
+
+
+	/**
+	 * When key is pressed, do something.
+	 *
+	 * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
+	 */
+	public void keyPressed(final KeyEvent e) {
+		this.getController().orderPerform(View.keyCodeToControllerOrder(e.getKeyCode()));
+	}
+		}
+
