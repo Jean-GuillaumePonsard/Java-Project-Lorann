b25cf6b14c09c1f67514fbc4392c19026b6b9615
Jean-GuillaumePonsard <jeanguillaume.ponsard@viacesi.fr>
2016-06-16 15:29:18 +0200

    Adding sprite into a specific folder and update model

--- model/src/main/java/model/ModelMainTest.java
+++ model/src/main/java/model/ModelMainTest.java
@@ -4,6 +4,7 @@ public class ModelMainTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		LorannGame lorannGame = new LorannGame();
 		System.out.println("FINISHED");
 	}
--- model/src/main/java/model/Sprite.java
+++ model/src/main/java/model/Sprite.java
@@ -1,6 +1,10 @@
 package model;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Sprite {
 
@@ -8,7 +12,7 @@ public class Sprite {
 	
 	public Sprite(String url)
 	{
-		//TEST
+		setImage(url);
 	}
 
 	public Image getImage() {
@@ -22,5 +26,12 @@ public class Sprite {
 	public void setImage(String url)
 	{
 		//Another method to set the Sprite of an object
+		try{
+			
+			image = ImageIO.read(new File("C:/Users/Jean-Guillaume P/Documents/Exia/Projet Java/Java/Java-Project-Lorann/model/src/main/resources/"+url));
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }
