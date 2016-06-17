08bcd7bf387d2c28a3ef99dd41c2f89bcc474ec4
FranzouilleWazaah <franz.alliod@viacesi.fr>
2016-06-17 09:56:15 +0200

    Details2

--- view/src/main/java/view/LorannView.java
+++ view/src/main/java/view/LorannView.java
@@ -1,8 +1,11 @@
 package view;
 
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.SwingUtilities;
 
+import contract.ControllerOrder;
 import contract.IController;
 import contract.IModel;
 import contract.IView;
@@ -27,7 +30,60 @@ public class LorannView implements IView, Runnable {
 		this.LorannviewFrame = new LorannViewFrame(model);
 		SwingUtilities.invokeLater(this);
 		}
-
+	/**
+	 * Method to perform Order when a key is pressed
+	 *
+	 * @param keyCode, keyCode2
+	 *          the key code
+	 */
+	
+	
+	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode, final int keyCode2) 
+	{
+		
+	
+		switch (keyCode) 
+		{
+			case KeyEvent.VK_UP:
+				switch(keyCode2)
+				{
+					//case KeyEvent.VK_LEFT: return ControllerOrder.UPPERLEFT;
+					//case KeyEvent.VK_RIGHT: return ControllerOrder.UPPERRIGHT;
+					default: return ControllerOrder.UP;
+				}
+			case KeyEvent.VK_DOWN:
+				switch(keyCode2)
+				{
+					//case KeyEvent.VK_LEFT: return ControllerOrder.DOWNLEFT;
+					//case KeyEvent.VK_RIGHT: return ControllerOrder.DOWNRIGHT;
+					default: return ControllerOrder.DOWN;
+				}
+			case KeyEvent.VK_LEFT:
+				switch(keyCode2)
+				{
+					//case KeyEvent.VK_UP: return ControllerOrder.UPPERLEFT;
+					//case KeyEvent.VK_DOWN: return ControllerOrder.DOWNLEFT;
+					default: return ControllerOrder.LEFT;
+				}
+			case KeyEvent.VK_RIGHT:
+				switch(keyCode2)
+				{
+					//case KeyEvent.VK_UP: return ControllerOrder.UPPERRIGHT;
+					//case KeyEvent.VK_DOWN: return ControllerOrder.DOWNRIGHT;
+					default: return ControllerOrder.RIGHT;
+				}
+				
+			case KeyEvent.VK_SPACE:
+				switch(keyCode2)
+				{
+					//case KeyEvent.VK_UP: return ControllerOrder.UPPERRIGHT;
+					//case KeyEvent.VK_DOWN: return ControllerOrder.DOWNRIGHT;
+					default: return ControllerOrder.LAUNCHSPELL;
+				}
+			}
+		return ControllerOrder.VOID;	
+		
+	}
 
 	/**
 	 * Sets the controller.
