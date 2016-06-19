a5e610e9da08b9f5f53ae64e19bd78807284a22d
FranzouilleWazaah <franz.alliod@viacesi.fr>
2016-06-19 17:42:32 +0200

    Added Tests for View

---
+++ view/src/test/java/view/TestLorannView.java
@@ -0,0 +1,34 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLorannView {
+
+	@Test
+	public void testLorannView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyCodeToControllerOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetControllerILorannController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetControllerIController() {
+		fail("Not yet implemented");
+	}
+
+}
