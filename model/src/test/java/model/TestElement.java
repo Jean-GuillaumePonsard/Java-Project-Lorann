b9cdac5fe4684ed22a67c7d12f9e40d2212bc8f1
Jean-GuillaumePonsard <jeanguillaume.ponsard@viacesi.fr>
2016-06-20 18:59:28 +0200

    Updating model tests

--- model/src/test/java/model/TestElement.java
+++ model/src/test/java/model/TestElement.java
@@ -15,6 +15,7 @@ import contract.Permeability;
  * The Class TestElement.
  *
  * @author Adrien Thevenet
+ * @version 16.06.2016
  */
 public class TestElement {
 
@@ -88,28 +89,28 @@ public class TestElement {
         	assertEquals(Permeability.PENETRABLE, element.getPermeability());
         }
 	}
-//	/*
-//	 * test method for {@link model.Element#getImage()}
-//	 * 
-//	 * to review
-//	 */
-//	@Test
-//	public void testGetImage() {
-//		model.Element element = new model.Element("http://i21.servimg.com/u/f21/15/31/14/80/ludort11.png", Permeability.BLOCKING);
-//		assertEquals("http://i21.servimg.com/u/f21/15/31/14/80/ludort11.png", element.getImage());
-//		}
-//	
-//	/*
-//	 * test method for {@link model.Element#setImage(String)}
-//	 * 
-//	 * to review
-//	 */
-//	@Test
-//	public void testSetImage() {
-//		model.Element element = new model.Element("http://i21.servimg.com/u/f21/15/31/14/80/ludort11.png", Permeability.BLOCKING);
-//		element.setImage("http://www.laboiteverte.fr/wp-content/uploads/2014/11/element-jeu-video-06.jpg");
-//		assertEquals("http://www.laboiteverte.fr/wp-content/uploads/2014/11/element-jeu-video-06.jpg", element.getImage());
-//	}
+	/*
+	 * test method for {@link model.Element#getImage()}
+	 * 
+	 * to review
+	 */
+	@Test
+	public void testGetImage() {
+		model.Element element = new model.Element("exia.png", Permeability.BLOCKING);
+		assertNotNull(element.getImage());
+		}
+	
+	/*
+	 * test method for {@link model.Element#setImage(String)}
+	 * 
+	 * to review
+	 */
+	@Test
+	public void testSetImage() {
+		model.Element element = new model.Element("exia.png", Permeability.BLOCKING);
+		element.setImage("exia.png");
+		assertNotNull(element.getImage());
+	}
 
 	/**
 	 * test method for {@link model.Element#getX()}
